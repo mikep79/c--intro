afb60b44a3fed6d2b89e7798628b662d888c826e
Mike Pettman <mp_palmatum@yahoo.com>
2017-11-29 18:12:44 -0600

    string console

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace c_sharp_intro
+namespace list_quickstart
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var names = new List<string> { "Mike", "Ana", "Felipe" };
+            foreach (var thing in names)
+            {
+                Console.WriteLine($"Hello {thing.ToLower()}!");
+            }
+            Console.WriteLine();
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Add("Mickey");
+            names.Remove("Ana");
+            names.Remove("Felipe");
+            names.Remove("Mike");
+            // {
+            //     Console.WriteLine($"Hello {name.ToUpper}!");
+            // }
+            // foreach (var thing in names)
+            // {
+                Console.WriteLine($"Hello {names[1].ToLower()}!");
+            // }
         }
     }
 }
